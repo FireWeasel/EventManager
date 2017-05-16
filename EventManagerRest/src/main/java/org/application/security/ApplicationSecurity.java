25070e48545007a5e31ea0a01f2c1a2b03756876
LuboYankov <ahhak123@gmail.com>
2017-05-16 20:20:54 +0200

    Adding CORS filter

--- EventManagerRest/src/main/java/org/application/security/ApplicationSecurity.java
+++ EventManagerRest/src/main/java/org/application/security/ApplicationSecurity.java
@@ -1,13 +1,21 @@
 package org.application.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
  
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
@@ -28,15 +36,45 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
  
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	http.csrf().disable();
+    	http.cors();
         http.authorizeRequests().antMatchers("/users/current/**").authenticated();
         http.authorizeRequests().antMatchers("/users/reservations/create/**").hasAuthority("USER");
         http.authorizeRequests().antMatchers("/users/tickets/create/**").hasAuthority("USER");
         http.authorizeRequests().antMatchers("/tickets/**").hasAuthority("EMPLOYEE");
         http.authorizeRequests().antMatchers("/reservations/**").hasAuthority("EMPLOYEE");
         http.authorizeRequests().antMatchers("/shops/**").hasAuthority("EMPLOYEE");
-        http.csrf().disable();
         http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
         http.formLogin().successHandler(authenticationSuccessHandler);
         http.formLogin().failureHandler(authenticationFailureHandler);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+    	List<String> allowedOrigins = new ArrayList<String>();
+    	allowedOrigins.add("*");
+    	List<String> allowedMethods = new ArrayList<String>();
+    	allowedMethods.add("POST");
+    	allowedMethods.add("PUT");
+    	allowedMethods.add("GET");
+    	allowedMethods.add("UPDATE");
+    	allowedMethods.add("DELETE");
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(allowedMethods);
+        List<String> allowedHeaders = new ArrayList<String>();
+        allowedHeaders.add("Authorization");
+        allowedHeaders.add("Cache-Control");
+        allowedHeaders.add("Content-Type");
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(allowedHeaders);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
