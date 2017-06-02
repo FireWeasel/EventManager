276ef113b5a76cfc3be4ae1895ea30ef8b7ff642
Vasil Sirakov <357733@student.fontys.nl>
2017-06-02 22:06:14 +0200

    Added camera functionality to main form.Check in app finished with maybe cosmetical changes to be made

--- C# Applications/CheckInApplication/CheckInApplication/CheckInForm.cs
+++ C# Applications/CheckInApplication/CheckInApplication/CheckInForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +10,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
 
 namespace CheckInApplication
 {
@@ -16,47 +19,112 @@ namespace CheckInApplication
         private RestClient rClient;
         private Ticket ticket;
         private User owner;
+        private IVideoSource videoSource;
+        private FilterInfoCollection videoDeviceList;
+        private volatile object _locker = new object();
         public CheckInForm(RestClient rClient)
         {
             this.rClient = rClient;
             InitializeComponent();
+
+            #region Populate combobox with available cameras and assigning NewFrame Event
+            videoDeviceList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
+            foreach (FilterInfo videoDevice in videoDeviceList)
+            {
+                cmbVideoSource.Items.Add(videoDevice.Name);
+            }
+            if (cmbVideoSource.Items.Count > 0)
+            {
+                cmbVideoSource.SelectedIndex = 0;
+            }
+            else
+            {
+                MessageBox.Show("No video sources found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            videoSource = new VideoCaptureDevice(videoDeviceList[cmbVideoSource.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            #endregion
         }
 
         private void btnCheckReservation_Click(object sender, EventArgs e)
         {
+            HidePictures();
+            if (videoSource != null && videoSource.IsRunning) // click twice = turn off camera
+            {
+                videoSource.SignalToStop();
+                pbCamera.Image = null;
+                btnCheckInTicket.Text = "Click to scan ticket";
+            }
+            else
+            {
+                #region Start video and timer
+                btnCheckInTicket.Text = "Stop scanning";
+                videoSource.Start();
+                timer1.Start();
 
-            CameraScanForm csf = new CameraScanForm(this);
-            csf.Show();
+                #endregion
+            }
+        }
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap  bitmap = (Bitmap)eventArgs.Frame.Clone();
+            //bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //bitmap.SetResolution(pbCamera.Width, pbCamera.Height);
+            pbCamera.Image = bitmap;
+        }
 
+        private void decode_QRtag()
+        {
+            try
+            {
+                //pictureBox1 shows the web cam video
+                Bitmap bitmap = new Bitmap(pbCamera.Image);
+                BarcodeReader reader = new BarcodeReader { AutoRotate = true, TryHarder = true };
+                Result result = reader.Decode(bitmap);
+                string decoded = result.ToString().Trim();
+                timer1.Stop();
+                videoSource.SignalToStop();
+                CompleteCheckIn(decoded);
+                btnCheckInTicket.Text = "Click to scan ticket";
+                pbCamera.Image = null;
+            }
+            catch
+            {
+            }
         }
 
         public void CompleteCheckIn(String id)
         {
             HidePictures();
-
             try
             {
                 ticket = this.rClient.GetTicket(Convert.ToInt64(id));
                 owner = rClient.GetUser(Convert.ToInt64(id));
                 if (ticket.CheckedIn)
-                {
+                { //if it was already checked in
                     MessageBox.Show("Ticket already checked in.");
                     pictureBox1.Visible = true;
+                    lblPaidReservation.Text = "Check in successful!";
                     lblPaidReservation.Visible = true;
                 }
                 else
                 {
+                    //checking in for the first time
                     rClient.CheckInTicket(ticket.Id);
                     pictureBox1.Visible = true;
+                    lblPaidReservation.Text = "Check in successful!";
                     lblPaidReservation.Visible = true;
                 }
                 UpdateListbox(owner);
             }
-            catch (WebException we)
+            catch (Exception) //web,format,argument
             {
-                MessageBox.Show("Check in unsuccessful!" + we.Message);
+                MessageBox.Show("Check in unsuccessful!");
                 pictureBox2.Visible = true;
-                lblReservationNotPayed.Visible = true;
+                lblPaidReservation.Text = "Check in unsuccessful!";
+                lblPaidReservation.Visible = true;
             }
         }
 
@@ -65,7 +133,6 @@ namespace CheckInApplication
             pictureBox1.Visible = false;
             pictureBox2.Visible = false;
             lblPaidReservation.Visible = false;
-            lblReservationNotPayed.Visible = false;
         }
 
         private void UpdateListbox(User user)
@@ -79,6 +146,19 @@ namespace CheckInApplication
         private void CampingForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             rClient.GetRequest("http://localhost:8080/logout");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            decode_QRtag();
+        }
+
+        private void CheckInForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+            }
         }
     }
 }
--- C# Applications/CheckInApplication/CheckInApplication/CheckInForm.designer.cs
+++ C# Applications/CheckInApplication/CheckInApplication/CheckInForm.designer.cs
@@ -28,9 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheckInForm));
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lblReservationNotPayed = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.lblPaidReservation = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -38,41 +37,23 @@
             this.lbUserInformation = new System.Windows.Forms.ListBox();
             this.btnCheckInTicket = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2.SuspendLayout();
+            this.pbCamera = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cmbVideoSource = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox3.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.lblReservationNotPayed);
-            this.groupBox2.Controls.Add(this.pictureBox2);
-            this.groupBox2.Controls.Add(this.lblPaidReservation);
-            this.groupBox2.Controls.Add(this.pictureBox1);
-            this.groupBox2.Location = new System.Drawing.Point(26, 109);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(581, 219);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            // 
-            // lblReservationNotPayed
-            // 
-            this.lblReservationNotPayed.AutoSize = true;
-            this.lblReservationNotPayed.Location = new System.Drawing.Point(352, 188);
-            this.lblReservationNotPayed.Name = "lblReservationNotPayed";
-            this.lblReservationNotPayed.Size = new System.Drawing.Size(160, 18);
-            this.lblReservationNotPayed.TabIndex = 3;
-            this.lblReservationNotPayed.Text = "Check in unsuccessful!";
-            this.lblReservationNotPayed.Visible = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(340, 23);
+            this.pictureBox2.Location = new System.Drawing.Point(366, 24);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(175, 162);
+            this.pictureBox2.Size = new System.Drawing.Size(194, 180);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -81,19 +62,19 @@
             // lblPaidReservation
             // 
             this.lblPaidReservation.AutoSize = true;
-            this.lblPaidReservation.Location = new System.Drawing.Point(47, 188);
+            this.lblPaidReservation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaidReservation.Location = new System.Drawing.Point(378, 238);
             this.lblPaidReservation.Name = "lblPaidReservation";
-            this.lblPaidReservation.Size = new System.Drawing.Size(144, 18);
+            this.lblPaidReservation.Size = new System.Drawing.Size(0, 20);
             this.lblPaidReservation.TabIndex = 1;
-            this.lblPaidReservation.Text = "Check in successful!";
             this.lblPaidReservation.Visible = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(38, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(366, 24);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(187, 162);
+            this.pictureBox1.Size = new System.Drawing.Size(194, 180);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -104,7 +85,7 @@
             this.groupBox3.Controls.Add(this.lbUserInformation);
             this.groupBox3.Location = new System.Drawing.Point(624, 23);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(251, 305);
+            this.groupBox3.Size = new System.Drawing.Size(251, 353);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Client information: ";
@@ -115,14 +96,14 @@
             this.lbUserInformation.ItemHeight = 18;
             this.lbUserInformation.Location = new System.Drawing.Point(13, 35);
             this.lbUserInformation.Name = "lbUserInformation";
-            this.lbUserInformation.Size = new System.Drawing.Size(227, 256);
+            this.lbUserInformation.Size = new System.Drawing.Size(227, 292);
             this.lbUserInformation.TabIndex = 0;
             // 
             // btnCheckInTicket
             // 
-            this.btnCheckInTicket.Location = new System.Drawing.Point(7, 20);
+            this.btnCheckInTicket.Location = new System.Drawing.Point(7, 268);
             this.btnCheckInTicket.Name = "btnCheckInTicket";
-            this.btnCheckInTicket.Size = new System.Drawing.Size(562, 47);
+            this.btnCheckInTicket.Size = new System.Drawing.Size(233, 63);
             this.btnCheckInTicket.TabIndex = 2;
             this.btnCheckInTicket.Text = "Click to scan QR code";
             this.btnCheckInTicket.UseVisualStyleBackColor = true;
@@ -130,42 +111,76 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbVideoSource);
+            this.groupBox1.Controls.Add(this.pbCamera);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.lblPaidReservation);
             this.groupBox1.Controls.Add(this.btnCheckInTicket);
+            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Location = new System.Drawing.Point(22, 13);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(585, 74);
+            this.groupBox1.Size = new System.Drawing.Size(585, 363);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // CampingForm
+            // pbCamera
+            // 
+            this.pbCamera.Location = new System.Drawing.Point(7, 24);
+            this.pbCamera.Name = "pbCamera";
+            this.pbCamera.Size = new System.Drawing.Size(272, 205);
+            this.pbCamera.TabIndex = 3;
+            this.pbCamera.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cmbVideoSource
+            // 
+            this.cmbVideoSource.FormattingEnabled = true;
+            this.cmbVideoSource.Location = new System.Drawing.Point(175, 235);
+            this.cmbVideoSource.Name = "cmbVideoSource";
+            this.cmbVideoSource.Size = new System.Drawing.Size(104, 26);
+            this.cmbVideoSource.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Available video devices:";
+            // 
+            // CheckInForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.ClientSize = new System.Drawing.Size(889, 349);
+            this.ClientSize = new System.Drawing.Size(889, 394);
             this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "CampingForm";
+            this.Name = "CheckInForm";
             this.Text = "Check in application";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CheckInForm_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CampingForm_FormClosed);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox3.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label lblReservationNotPayed;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label lblPaidReservation;
         private System.Windows.Forms.PictureBox pictureBox1;
@@ -173,6 +188,10 @@
         private System.Windows.Forms.ListBox lbUserInformation;
         private System.Windows.Forms.Button btnCheckInTicket;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbCamera;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbVideoSource;
     }
 }
 
